070f73772918e42c65299230a8c66fb3da62baf2
RRRobert212 <robertwd1@gmail.com>
2023-11-26 18:38:21 -0800

    Total rewrite, it still doesn't work, but it's closer. Need to get it to only look at links in the actual body of the text. Also for some reason it just occasionally breaks on certain articles

--- Wikipedia_Fisrt_Link.py
+++ Wikipedia_Fisrt_Link.py
@@ -1,82 +1,50 @@
-#A program that opens a random wikipedia article and then clicks the first link until it arrives at the philosophy article
-#It should print the first article, the number of steps it took to get to philosophy, and perhaps the names of the articles inbetween
-#Can eventually add some sort of graph or something to display relationships between articles
-
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from bs4 import BeautifulSoup
-import time
-import random
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def open_random_wikipedia_article():
-    # Specify the path to the Firefox binary
-    firefox_binary_path = '/usr/bin/firefox'
-
-    # Create Firefox options
-    firefox_options = Options()
-    firefox_options.binary_location = firefox_binary_path
-
-    # Create a Firefox WebDriver with the specified options
-    driver = webdriver.Firefox(options=firefox_options)
-
-    # Open a random article on Wikipedia
-    driver.get("https://en.wikipedia.org/wiki/Special:Random")
-
+    driver = webdriver.Chrome()
+    driver.get("https://en.wikipedia.org/wiki/John_Locke")
     return driver
 
-def find_next_link(driver):
-    # Wait for the page to load
-    time.sleep(2)
+def finder(driver):
+    while True:
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
 
-    # Get the page source
-    page_source = driver.page_source
+        title_tag = soup.find('title')
+        print(title_tag.text.strip())
 
-    # Use BeautifulSoup to parse the HTML
-    soup = BeautifulSoup(page_source, 'html.parser')
+        if title_tag.text.strip() == "Philosophy - Wikipedia":
+            print("Reached Philosophy page!")
+            break
 
-    # Find all non-italicized and non-paranthesized links
-    candidate_links = soup.find_all('a', {'class': False, 'href': True})
-    valid_links = [link for link in candidate_links if not is_italicized(link) and not is_paranthesized(link)]
+        link = soup.findAll('a', class_="mw-redirect")
 
-    if not valid_links:
-        # If no valid links are found, close the browser and return None
-        driver.quit()
-        return None
+        print(link)
 
-    # Choose a random valid link
-    random_link = random.choice(valid_links)
+        if link:
+            link_xpath = "//a[@class='mw-redirect' and @href='" + link[1].get('href') + "']"
+            # Wait for the link to be clickable
+            link_element = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, link_xpath))
+            )
 
-    # Click the link
-    random_link.click()
+            link_element.click()
 
-    return driver
+            # Wait for the page to be updated
+            WebDriverWait(driver, 10).until(EC.staleness_of(link_element))
 
-def is_italicized(link):
-    return link.find('i') is not None or link.find('em') is not None
+        else:
+            print("No link found with the specified criteria.")
 
-def is_paranthesized(link):
-    return link.find('span', {'class': 'mw-editsection-bracket'}) is not None
+def main():
+    driver = open_random_wikipedia_article()
+    finder(driver)
 
 if __name__ == "__main__":
-    # Open a random article
-    driver = open_random_wikipedia_article()
-
-    # Specify the title of the article you want to reach
-    target_article = "Your Target Article Title"
-
-    # Loop until the target article is reached
-    while True:
-        # Find the next link on the current page
-        driver = find_next_link(driver)
-
-        if driver is None:
-            print("No valid links found. Exiting.")
-            break
-
-        # Check if the current article is the target article
-        if target_article.lower() in driver.title.lower():
-            print(f"Target article '{target_article}' reached. Exiting.")
-            break
-
-    # Close the browser
-    driver.quit()
+    main()
